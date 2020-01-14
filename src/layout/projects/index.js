4bd78fcc03d06e1a344d7417d64174a887b2ff0a
Vitaly <v.ignatenko360@gmail.com>
2020-01-14 13:44:47 +0300

    rt2

--- src/layout/projects/index.js
+++ src/layout/projects/index.js
@@ -2,9 +2,46 @@ import './projects.sass';
 import Swiper from 'swiper/js/swiper.min.js';
 
 
-var swiper = new Swiper('.header-swiper', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+var swiperContent = new Swiper('.swiper_content', {
+  direction: 'vertical',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+var swiperTro = new Swiper('.swipertop', {
+  initialSlide: 1,
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows : true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
+var swiperBottom = new Swiper('.swiperbottom', {
+  initialSlide: 1,
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows : true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
   },
 });
